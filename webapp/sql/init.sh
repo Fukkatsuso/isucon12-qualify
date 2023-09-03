11db677a391f3bc43bc0e9ce79f5d196670fc36d
Fukkatsuso <tatsuki.mutsuro@dena.com>
2023-09-03 17:14:01 +0900

    fix: sql/init.sh

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -10,16 +10,11 @@ ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
 # MySQLを初期化
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < admin/01_create_mysql_database.sql
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < admin/10_schema.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < admin/10_schema.sql
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
