1636cc521370ff0dd29708447bdfc13bd04ddab3
Fukkatsuso <tatsuki.mutsuro@dena.com>
2023-09-03 18:48:12 +0900

    fix sql/init.sql

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -3,5 +3,5 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
--- change admin schema
-CREATE INDEX tenant_id_and_competition_id_idx ON visit_history (tenant_id, competition_id);
+-- change admin schema (execute once)
+-- CREATE INDEX tenant_id_and_competition_id_idx ON visit_history (tenant_id, competition_id);
