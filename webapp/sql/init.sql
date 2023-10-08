bcff940e9b6dc48fe8841174f4fe3d7cf06ad8b6
Fukkatsuso <tatsuki.mutsuro@dena.com>
2023-10-08 23:13:41 +0900

    fix: sql

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -6,7 +6,7 @@ ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 -- change admin schema (execute once)
 -- CREATE INDEX tenant_id_and_competition_id_idx ON visit_history (tenant_id, competition_id);
 
-DELETE FROM visit_history
+DELETE visit_history
 WHERE created_at > (
   SELECT MIN(tmp.created_at)
   FROM visit_history tmp
